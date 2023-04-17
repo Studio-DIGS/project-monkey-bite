578544a2cfeda94f73344446481432af7263eb0e
Noah Krim <nkrim62@gmail.com>
2023-04-17 12:50:09 -0700

    More robust logic for combos in ProtagArmedAttack

--- Assets/Scripts/Gameplay/ProtagScripts/ProtagSM/States/Combat/Armed/ProtagArmedAttack.cs
+++ Assets/Scripts/Gameplay/ProtagScripts/ProtagSM/States/Combat/Armed/ProtagArmedAttack.cs
@@ -58,12 +58,13 @@ public class ProtagArmedAttack : ProtagState
     private bool TryTransitionOut()
     {
         bool animationFinished = animationController.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9 && animationController.GetCurrentAnimatorStateInfo(0).IsTag("Attack");
-        bool maxTimePassed = stateMachine.CurrentStateDuration > animationController.GetCurrentAnimatorStateInfo(0).length + 0.2f; 
+        bool maxTimePassed = stateMachine.CurrentStateDuration > animationController.GetCurrentAnimatorStateInfo(0).length + 0.2f;
         
-        // If animationFinished and player inputting attack, cycle back to attack state
-        // Or if no inputting attack, end combo and go back to idle
-        return animationFinished && transitions.ToCombatSelector()
-                || maxTimePassed && EndCombo() && transitions.ToMovementSelector();
+        return
+            // If animationFinished and next player input is also an attack, continue combo
+                animationFinished && transitions.ToCombatSelector() && (stateMachine.PreviousState == this || EndCombo()) 
+            // Or, if full attack time completed, end combo and transition out
+               || maxTimePassed && EndCombo() && transitions.ToMovementSelector();
     }
 
     private bool EndCombo()

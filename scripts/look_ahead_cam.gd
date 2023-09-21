cede42d167531d936449098fd7ce1962374f40ed
jucobee <jucobee@gmail.com>
2023-09-21 01:59:07 -0700

    continued configuring

--- scripts/look_ahead_cam.gd
+++ scripts/look_ahead_cam.gd
@@ -1,5 +1,18 @@
 extends Camera3D
 
+@export var post_processing := true:
+	set(p):
+		if p:
+			$Postprocess.show()
+			post_processing = p
+			var a = Vector3(-1, 1, 0).normalized()
+			var b = Vector3(1, 0, 0).normalized()
+			print("dot: ", a.dot(b))
+		else:
+			$Postprocess.hide()
+			post_processing = p
+
+
 # Camera settings
 @export var camera_speed = 2.5
 @export var look_ahead_distance = 4.0

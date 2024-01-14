c6a6157190cc5ed8ef3ce024ab7a9672c88d55be
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2024-01-14 15:27:09 -0800

    Integrated dialogue system and NPC template. Press "E" to interact with NPC Press "enter" to continue dialogue

--- common/game.gd
+++ common/game.gd
@@ -1,3 +1,15 @@
 extends Node3D
 
+#@export_group("Worlds List")
+#@export var World1: PackedScene
+var preloadScene = preload("res://levels/test_scene_2.tscn")
 
+func _process(_delta):
+	if (Input.is_action_just_pressed("next level")):
+#		if World1 != null:
+#			get_tree().change_scene(World1)
+#			print("next world")
+#		else:
+#			print("World 1 is Null")
+		get_tree().change_scene_to_packed(preloadScene)
+		print("Loading next world...")

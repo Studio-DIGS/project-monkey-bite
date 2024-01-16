c7f4402241a801cfbe2d0ed9868b99a86e51ca00
jucobee <jucobee@gmail.com>
2024-01-15 16:31:26 -0800

    combo doesn't start till contact with enemy

--- player/states/player_attack.gd
+++ player/states/player_attack.gd
@@ -4,6 +4,8 @@ var combo_counter = 0
 var max_combo = 0
 var combo: Array[AttackResource]
 var apply_gravity = false
+var orientation = 1
+var contact = false 
 
 func enter(msg := {}):
 	combo = player.combo
@@ -15,7 +17,7 @@ func enter(msg := {}):
 	print(combo_counter)
 	
 	# configure hitbox to correct AttackResource
-	curr_attack.knockback.x *= player.orientation
+	curr_attack.knockback.x *= orientation
 	player.hitbox.configure_hitbox(curr_attack)
 	
 	# plays the initial animation
@@ -33,16 +35,25 @@ func physics_update(delta):
 	player.move_and_slide()
 	
 	# queue the next attack
-	if combo_counter < max_combo and not player.anim.get_queue():
+	if combo_counter < max_combo and contact and not player.anim.get_queue():
 		if Input.is_action_just_pressed("attack"):
 			combo_counter += 1
+			contact = false
 			player.anim.queue(player.combo[combo_counter].animation)
+
 
 
 func _on_animation_player_animation_finished(_anim_name):
 	combo_counter = 0
 	state_machine.transition_to("Idle")
 
-
 func _on_animation_player_animation_changed(old_name, new_name):
 	state_machine.transition_to("Attack")
+
+
+func _on_player_turn_around():
+	orientation *= -1
+
+# check if made contact with enemy
+func _on_hitbox_area_entered(area):
+	contact = true

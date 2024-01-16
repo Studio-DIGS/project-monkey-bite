a28207e858b6506c319a2529f3ae12f49fcb0b3b
jucobee <jucobee@gmail.com>
2024-01-15 16:37:49 -0800

    player can only try to attack once in the air

--- player/states/player_air.gd
+++ player/states/player_air.gd
@@ -1,5 +1,7 @@
 extends PlayerState
 
+var attacks: int
+
 func enter(msg := {}):
 	if msg.has("do_jump"):
 		player.velocity.y = player.jump_height
@@ -13,9 +15,11 @@ func physics_update(delta):
 	player.velocity.x = lerp(player.velocity.x, player.hori_input * player.speed, delta * player.accel)
 	player.move_and_slide()
 	
-	if Input.is_action_pressed("attack"):
+	if attacks > 0 and Input.is_action_pressed("attack"):
+		attacks -= 1
 		state_machine.transition_to("Attack", {air = true})
 	
 	# Landing
 	elif player.is_on_floor():
+		attacks = 1 # reset number of air attacks
 		state_machine.transition_to("Land")

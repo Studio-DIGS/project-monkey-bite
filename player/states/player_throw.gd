f608c34694d2fc6991a0dfd372a10dc42e1ddd50
jucobee <jucobee@gmail.com>
2024-04-14 13:51:16 -0700

    transitions setup

--- player/states/player_throw.gd
+++ player/states/player_throw.gd
@@ -1,13 +1,32 @@
 extends PlayerState
+var slide_velocity: float
 
 func enter(_msg := {}):
+	slide_velocity = player.velocity.x * 0.5
+	
 	player.anim.play("Throw")
 
 func physics_update(delta):
-	pass
+	player.velocity.x = lerp(player.velocity.x, slide_velocity, delta * player.accel)
+	player.move_and_slide()
 
 
 
 func _on_animation_player_animation_finished(_anim_name):
-	state_machine.transition_to("Idle")
+	var run = false
+	if state_machine.state.name == "Throw":
+		if player.hori_input != 0:
+			run = true
+			player.anim.play("Run")
+		else:
+			player.anim.play("Idle")
+		
+		# wait for animation to finish blending to idle before fully transitioning state
+		var blend_time = player.anim.playback_default_blend_time
+		await get_tree().create_timer(blend_time).timeout
+		
+		if run:
+			state_machine.transition_to("Run")
+		else:
+			state_machine.transition_to("Idle")
 

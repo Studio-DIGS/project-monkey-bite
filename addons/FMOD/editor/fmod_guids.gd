ab446a821873e3da6e86d605970ef559e2cdbf69
Ryan Cheung <ry.adam.cheung@gmail.com>
2024-04-14 19:00:26 -0700

    help meeeeeee this damn shit isnt working
    fmod banks are working, but why the FUCK isn't audio playing?

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,21 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'AsperitasAudio.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const MUSIC = "{1891d5b9-bcb3-477a-862f-0ef5fb3ef220}"
+
+class Busses:
+	const MASTER_BUS = "{57fcde9d-5aad-4426-9485-90eb5df0632b}"
+	const REVERB = "{dc351256-be44-4b7e-9f10-70fcdfda6890}"
+
+class Banks:
+	const MASTER = "{70200d6d-0c51-4ffd-bb2d-4fd8ddc34ea1}"
+
+class Parameters:
+	const MUSIC_LEVEL = "{30a16e53-b6bb-4c4d-8a08-9904dd7623df}"
+
